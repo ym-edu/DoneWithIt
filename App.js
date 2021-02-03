e76fc978f68d96e8761823397fe26fa19b8914e8
nym <nym@nym.local>
2021-02-02 20:31:58 -0600

    Testing physical device connection with Expo Go

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("Testing")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Mobile World!</Text>
       <StatusBar style="auto" />
     </View>
   );
