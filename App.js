998803aa2c4ebda2b61c25fe07c3063a70633eca
nym <nym@nym.local>
2021-02-07 16:06:37 -0600

    Flex position absolute

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <View style={styles({color: "dodgerblue"}).container}>
       <View style={styles({color: "gold", width: 300}).section}/>
-      <View style={styles({color: "silver"}).section}/>
+      <View style={styles({color: "silver", position: "absolute", x: 20, y: 50}).section}/>
       <View style={styles({color: "violet"}).section}/>
       <View style={styles({color: "tomato"}).section}/>
       <View style={styles({color: "green"}).section}/>
@@ -31,5 +31,8 @@ const styles = (props) => StyleSheet.create({
     width: 100,
     height: props.width ? props.width : 100,
     backgroundColor: props.color,
+    position: props.position ? props.position : "relative",
+    top: props.x ? props.x : 0,
+    left: props.y ? props.y : 0,
   }
 });
