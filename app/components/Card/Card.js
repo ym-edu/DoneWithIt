d87caf5b87fab108c41b48a8301fa1c0a4417b1f
nym <nym@nym.local>
2021-02-16 12:31:23 -0600

    Created a data.js file that contains an array of 'exercises' with hardcoded properties to simulate a database; Added logic to decide how to render the subtitle based on the default mode of an exercise; Like button is functional and will change state on press. Also liked state resets to value defined in 'database'

--- app/components/Card/Card.js
+++ app/components/Card/Card.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import { Thumbnail, Pressables, TextDescription } from './index'
 
-function Card({media, title, subtitle, colors}) {
-  const handleLike = () => alert('Added to liked exercises')
+function Card({media, title, subtitle, colors, liked}) {
+  const [like, setLike] = useState(liked)
+  console.log('Card' ,like)
+
+  const handleLike = () => {
+    setLike(!like);
+  }
+
+  useEffect(() => {
+    setLike(liked)
+  }, [title])
+  // When changing itemNumber from Sandbox, the liked prop being passed to card had to change, however sometimes the value coincide.
+  // Therefore: if this item's liked prop was FALSE, its value was changed to TRUE by the handler.
+  // And: if I changed the item from Sandbox to an item which's liked prop was TRUE, useEffect doesn't update state because the liked prop didn't seem to change (from useEffects perspective).
+  // So: whenever I switched back to the previous item which was liked (through handler), it's value never reset to its original value from the data object.
+  // The fix was to set useEffect to 'watch' for changes from another parameter that did not share values from the data object.
+  // The chosen parameter was title.
+  // Now: changing itemNumber to select the exercise resets the liked button to the value defined in 'database' (data object used to simulate a database).
+  // Also: Handler changes liked value as it should and is overritten when selecting a new exercise as described above
+
   const handleOptions = () => console.log('Open options')
 
   return (
@@ -18,7 +36,7 @@ function Card({media, title, subtitle, colors}) {
       </View>
 
       <View>
-        <Pressables onPress={{like: handleLike, options: handleOptions}} colors={colors}/>
+        <Pressables onPress={{like: handleLike, options: handleOptions}} colors={colors} like={like}/>
       </View>
     </View>
   );
--- app/screens/Sandbox.js
+++ app/screens/Sandbox.js
@@ -2,23 +2,37 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import colors from '../config/colors'
+import data from '../config/data'
 import Card from '../components/Card/Card'
 
 
 function Sandbox() {
-  const data = {
-    // title: `balance trainer single leg kneeling good morning`,
-    title: `high knees running in place`,
-    subtitle: `8 reps`
+  const itemNum = 0;
+  console.log('=============TESTING================')
+  console.log('Sandbox', data[itemNum].liked);
+
+  const renderSwitch = (item) => {
+    switch(item.mode) {
+      case 'r1':
+      case 'r2':
+        return `${item.reps} reps`;
+        break;
+      case 't1':
+      case 't2':
+        return `${item.min} min ${item.sec} sec`
+      default:
+        alert('NAN');
+    }
   }
 
   return (
       <View style={styles().screen}>
         <Card
         media={'../../assets/thumbnail.jpg'}
-        title={data.title}
-        subtitle={data.subtitle}
+        title={data[itemNum].title}
+        subtitle={renderSwitch(data[itemNum])}
         colors={colors}
+        liked={data[itemNum].liked}
         />
       </View>
   );
