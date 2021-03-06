3429692de1882c09c823a440ba93724bad0e7246
nym <nym@nym.local>
2021-03-05 21:17:54 -0600

    MAGIC

--- app/components/Controls/Controls.js
+++ app/components/Controls/Controls.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Previous, Next, Reset, Counter } from './'
 
-function Controls({data, id}) {
+function Controls({exercises, currentExercise: {data, id}}) {
+  const allExercises = exercises;
+  const [touch, setTouch] = useState([]);
+  useEffect(() => {
+    if(allExercises.length > 0) {
+      const initialState = allExercises.map(exercise => exercise.isDisabled);
+      setTouch(initialState);
+      console.log(initialState)
+    }
+  }, [allExercises])
+
+  const handleReset = () => {
+    const newArr = [...touch]; // avoid writing over state
+    newArr[id-1] = !newArr[id-1]; // find array item ID:currentExerciseId-1 (-1 to match index since index(0)=id(1)) and set to new value (flip boolean, in this case)
+    setTouch(newArr); // update state setting new array with updated state for selected index
+  }
+
+
+
+
 
   function formatTime(min, sec) {
     return`${min > 10 ? min : `0${min}`}:${sec > 10 ? sec : `0${sec}`}`
@@ -26,14 +45,14 @@ const onPress = (component) => {
         <Previous
         onPress={() => onPress('Previous')} woStarting={false}/>
 
-        <Counter mode={data.mode} item={display} onPress={() => console.log(`DATA ID: ${id} |`, data)} />
+        <Counter mode={data.mode} item={display} onPress={() => console.log(`DATA ID: ${id} |`, data, touch[id-1])} />
 
         <Next
-        onPress={() => onPress('Next')} woEnding={false}/>
+        onPress={() => console.log(touch)} woEnding={false}/>
       </View>
 
       <Reset
-      onPress={() => onPress('Reset')} exStarting={false}/>
+      onPress={handleReset} exStarting={false}/>
     </View>
   );
 }
--- app/screens/Train.js
+++ app/screens/Train.js
@@ -6,25 +6,28 @@ import Controls from '../components/Controls/Controls';
 import { workout } from '../config'
 
 function Train() {
+  workout.forEach(obj => obj.isDisabled = false)
   // const startingExercise = workout[0];
   // const endingExercise = workout[workout.length - 1];
-  
+
+// start
   const currentExercise = workout[9]
 
-  const { data } = currentExercise;
-  const nextExercise = workout[currentExercise.id];
+//   // const { data } = currentExercise;
+//   // const nextExercise = allExercises[currentExercise.id];
 
-  function RenderPreview() {
-    return (
-      currentExercise.id >= workout.length ? null : <Preview item={nextExercise}/>)
-  }
+//   // function RenderPreview() {
+//   //   return (
+//   //     currentExercise.id >= allExercises.length ? null : <Preview item={nextExercise}/>)
+//   // }
+// end
 
   return (
     <View style={styles.screen}>
       <Video item={currentExercise}/>
-      <Controls data={data} id={currentExercise.id}/>
-      <Description item={currentExercise}/>
-      <RenderPreview/>
+      <Controls exercises={workout} currentExercise={currentExercise}/>
+      {/* <Description item={currentExercise}/>
+      <RenderPreview/> */}
     </View>
   );
 }
