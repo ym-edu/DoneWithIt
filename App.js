5885b6bf98a9a8b967e08632ebc4db702e2bb238
nym <nym@nym.local>
2021-03-18 13:16:11 -0600

    Installed react-navigation and it's dependancies + Tab & Stack navigators; Restructured screens so each Tab folder can contain corresponding Stack screens; Create & Train screens were moved to temp folder

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
 import Layout from './app/layout';
-import { Create } from './app/screens'
+import { Explore, Library, Log } from './app/screens'
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
     <Layout>
-      <Create/>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Explore">
+          <Tab.Screen name="Explore" component={Explore} />
+          <Tab.Screen name="Log" component={Log} />
+          <Tab.Screen name="Library" component={Library} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </Layout>
   );
 }
---
+++ app/screens/Library/LibraryTab.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function Library() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Library Screen</Text>
+    </View>
+  );
+}
+
+export default Library;
