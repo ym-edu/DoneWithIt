bf520a5562d46bc57e4d88a3bdb3d265bed95b5d
nym <nym@nym.local>
2021-03-22 10:50:42 -0600

    Restructured RootStack as 3 conditionally rendered routed screens for Loading||AppTabs||AuthStack

--- app/routes/Navigation.js
+++ app/routes/Navigation.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 // import AppTabs from './AppTabs';
-import AuthStack from './AuthStack'
 import RootStack from './RootStack'
-import Loading from '../screens/Loading'
 
 const Theme = { //TODO: Toggle dark mode through device system settings
   ...DefaultTheme,
@@ -15,25 +13,9 @@ const Theme = { //TODO: Toggle dark mode through device system settings
 }
 
 export default function Navigation() {
-  const [isLoading, setIsLoading] = React.useState(true)
-  const [user, setUser] = React.useState(null);
-
-  React.useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(!isLoading) //Toggles Loading vs AuthStack per render
-      // setUser({}) //TODO: Figure out how to set user
-      // After LogIn || SignUp
-    }, 500)
-
-    // setTimeout(() => {
-    //   setUser({})
-    // }, 1000)
-  }, [])
-
   return (
     <NavigationContainer theme={Theme}>
-      {isLoading ? <Loading/>
-      : user ? <RootStack/> : <AuthStack/>}
+      <RootStack/>
     </NavigationContainer>
   );
 }
--- app/routes/RootStack.js
+++ app/routes/RootStack.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import AppTabs from './AppTabs'
+import AppTabs from './AppTabs';
+import Loading from '../screens/Loading';
+import AuthStack from './AuthStack'
 
 const Stack = createStackNavigator();
 
 export default function RootStack() {
+  const [isLoading, setIsLoading] = React.useState(true)
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(!isLoading) //Toggles Loading vs AuthStack per render
+      setUser({}) //TODO: Figure out how to set user
+      // After LogIn || SignUp
+    }, 500)
+  }, [])
+
   return (
-      <Stack.Navigator>
-        <Stack.Screen name="Root" component={AppTabs}/>
-      </Stack.Navigator>
+    <Stack.Navigator>
+      {isLoading
+      ? <Stack.Screen name="Loading" component={Loading} />
+      : user
+        ? <Stack.Screen name="AppTabs" component={AppTabs} />
+        : <Stack.Screen name="AuthStack" component={AuthStack} />}
+    </Stack.Navigator>
   );
 }
