a66ee0b6556fb007be9a8976df00aab84c6cc370
nym <nym@nym.local>
2021-03-29 12:05:43 -0600

    Upgraded to RNfb/app 11.2.0 to keep all firebase module versions consistent; Created firebase.json file in root dir; Updated build.gradle dependancies (gradle:3.5.3 to gradle:3.5.4) to get app to run correctly after installing admob; Configured admob Requests in App.js; Tested: Banner ad in Log screen & Interstitial ad in LogIn screen when user logs in.

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import Layout from './app/layout';
 import Navigation from './app/routes/Navigation';
+import admob, { MaxAdContentRating } from '@react-native-firebase/admob';
 
 export default function App() {
+  React.useEffect(() => {
+  admob()
+    .setRequestConfiguration({
+      // Update all future requests suitable for parental guidance
+      maxAdContentRating: MaxAdContentRating.PG,
+  
+      // Indicates that you want your content treated as child-directed for purposes of COPPA.
+      tagForChildDirectedTreatment: true,
+  
+      // Indicates that you want the ad request to be handled in a
+      // manner suitable for users under the age of consent.
+      tagForUnderAgeOfConsent: true,
+    })
+    .then(() => {
+      // Request config successfully set!
+    });
+  }, [])
+  
   return (
     <Layout>
       <Navigation/>
--- app/screens/LogIn.js
+++ app/screens/LogIn.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import { Button } from 'react-native';
 import analytics from '@react-native-firebase/analytics';
+import { InterstitialAd, TestIds, AdEventType } from '@react-native-firebase/admob';
 
 import { useAuthUpdate } from '../hooks/useAuth'
 
+const adUnitId = __DEV__ ? TestIds.INTERSTITIAL : 'ca-app-pub-2742026173933447~7286128693';
+
+const interstitial = InterstitialAd.createForAdRequest(adUnitId, {
+  requestNonPersonalizedAdsOnly: true,
+  keywords: ['fitness', 'exercie'],
+});
+
 function LogIn({ navigation }) {
+
   const { logIn } = useAuthUpdate();
+
+  const [loaded, setLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    const eventListener = interstitial.onAdEvent(type => {
+      if (type === AdEventType.LOADED) {
+        setLoaded(true);
+      }
+    });
+
+    // Start loading the interstitial straight away
+    interstitial.load();
+
+    // Unsubscribe from events on unmount
+    return () => {
+      eventListener();
+    };
+  }, []);
+
+  // No advert ready to show yet
+  if (!loaded) {
+    return null;
+  }
 
   return (
     <>
-      <Button title="Log In" onPress={() => logIn()} />
+      <Button title="Log In" onPress={() => {
+        interstitial.show();
+        logIn()
+      }} />
       <Button title="Sign Up" onPress={() => navigation.navigate("SignUp")} />
       <Button title="Forgot Password?" onPress={() => navigation.navigate("ResetPassword")} />
       <Button
