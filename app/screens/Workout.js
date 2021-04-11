cef77ba2bc7714b49996de9061da73aac8f2cf2d
nym <nym@nym.local>
2021-04-11 12:31:41 -0500

    UPDATED Workout to render exercises in flatlist; UPDATED exercises in backend to take a workouts field containing 'workoutId' map key and the exercises index within that workout as a value; This configuration on backend allows me to query exercises where workouts.'workoutId' field exists (exercise belongs to 'workoutId'), using orderBy in it's default ascending order (effectively ordering by index)

--- app/screens/Workout.js
+++ app/screens/Workout.js
@@ -1,14 +1,67 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 import CreateButton from '../components/CreateButton'
 import Spacer from '../components/Spacer';
+import firestore from '@react-native-firebase/firestore'
+import ExerciseCard from '../components/ExerciseCard'
 
 function Workout({ navigation, route }) {
   const routeData = route.params.id
+
+  const userId = 'user-1';
+  const [exercises, setExercises] = useState(null)
+
+  const selectSubtitle = (data) => {
+    switch(data.mode) {
+      case 'r1':
+      case 'r2':
+        return `${data.reps} reps`;
+      case 't1':
+      case 't2':
+        return (
+          `${data.min === 0 ? '' : `${data.min} min `}${data.sec === 0 ? '' : `${data.sec} sec`}`
+        )
+      default:
+        alert('NAN');
+    }
+  }
+
+  useEffect(() => {
+    const fetchExercises = () => {
+      firestore().collection("users").doc(userId).collection("exercises")
+      .orderBy(`workouts.${routeData}`)
+      .onSnapshot(snapshot => {
+        const exerciseDocs = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log(exerciseDocs.length) //TODO: Save state for parent to determine count
+        setExercises(exerciseDocs)
+      })
+    };
+    fetchExercises()
+    return () => {
+      fetchExercises()
+    }
+  }, [])
   
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{routeData}</Text>
+      <FlatList style={styles.content}
+          data={exercises}
+          keyExtractor={data => data.id.toString()}
+          renderItem={({ item }) => (
+            <ExerciseCard
+              url={item.video.url}
+              title={item.exName}
+              subtitle={selectSubtitle(item.data)}
+              onPress={() => null}
+            />
+          )}
+          ItemSeparatorComponent={() => <Spacer mV={8}/>}
+          showsVerticalScrollIndicator={false}
+        />
+      {/* <Text style={styles.text}>{routeData}</Text> */}
       <Spacer mV={16}
       style={{width: '100%', borderTopWidth: 1, borderTopColor: '#383B3B',}}/>
       <CreateButton icon={'plus'} title='add exercises' onPress={() => navigation.navigate("Modal")}/>
@@ -22,6 +75,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'flex-end',
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+  content: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    // backgroundColor: 'pink',
   },
   text: {
     color: 'white'
