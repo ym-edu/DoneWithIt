6c98ba3d290381b33bc8db8a923ee097c16d7e88
nym <nym@nym.local>
2021-04-30 22:26:49 -0500

    Re-did video loop: Not exactly sure what I did (trial and error), but it finally loops itself; initialPlayerParams end parameter works at every iteration, while start only works on the first; Only tradeoff is that video is unpausable

--- app/components/SetVideo.js
+++ app/components/SetVideo.js
@@ -1,124 +1,48 @@
-import react from "react";
 import React, { useState, useCallback, useRef, useEffect } from "react";
-import { StyleSheet, View, Button, Text } from 'react-native'
-import { ScrollView } from "react-native-gesture-handler";
+import { StyleSheet, View } from 'react-native'
 import YoutubePlayer from "react-native-youtube-iframe";
-import MultiSlider from '@ptomasroos/react-native-multi-slider'
-import formatTime from '../temp/formatTime'
 
-function Video({url='ZfawH9NsTtI', start, end}) {
-  const scrollEnabled = React.useRef(false)
-  const enableScroll = () => {
-    scrollEnabled.current = true
-  }
-  const disableScroll = () => {
-    scrollEnabled.current = false
-  }
-  
+function Video({url='DN0gAQQ7FAQ'}) {
   const playerRef = useRef();
-  const [ready, setReady] = useState(false)
-  
-  const [duration, setDuration] = useState(1)
-  const [startState, setStartState] = useState(0)
-  const [endState, setEndState] = useState(duration)
-  
-  function onReady() {
-    setReady(!ready)
-  }
-  
-  
+  const [playing, setPlaying] = useState(true);
+  const [playerState, setPlayerState] = useState('');
+
+  const togglePlaying = useCallback(() => {
+    return !playing
+  }, [playing])
+
   const onStateChange = useCallback((state) => {
-    console.log(state)
-    if(state === 'ended') {
-      setReady(!ready)
-    }
-    //WARNING: without the duration dependancy, BEFORE & AFTER will continue to log duration as 0
-    // console.log("BEFORE",duration)
-    // if(duration > 0) return
-    // console.log("AFTER",duration)
-
-    if(state === 'playing' && duration <= 1) {
-      // console.log('onStateChange', state)
-      
-      playerRef.current?.getDuration()
-      .then(duration => {
-        // console.log("Duration", duration)
-        setDuration(Math.floor(duration))
-        setEndState(Math.floor(duration))
-      });
-    }
-  }, [ready, duration])
-
-  useEffect(() => {
-    playerRef.current.seekTo(startState, true) //Play video
-    const load = setTimeout(() => {
-      playerRef.current.seekTo(startState+1, true) //After 1s seekTo()
-    }, 1300); //300 + compensation to look more seemless
-    
-    return () => clearTimeout(load)
-  }, [ready])
-
-  const onValuesChange = useCallback((state) => {
-    playerRef.current.seekTo(state[0], true)
-    setStartState(state[0])
-    setEndState(state[1])
-    setReady(!ready)
+    setPlayerState(state)
   }, [])
 
+  useEffect(() => {
+    if(playerState === "ended" || playerState === "paused") {
+      setPlaying(togglePlaying())
+    }
+  }, [playerState])
+
+  useEffect(() => {
+    if(playerState === "ended" || playerState === "paused") {
+      setPlaying(true)
+    }
+  }, [togglePlaying])
+
+ 
   return (
     <>
     <View style={styles.container}>
       <YoutubePlayer
-        videoId={url}
-        initialPlayerParams={{
-          start: start,
-          end: endState,
-        }}
-        ref={playerRef}
-        height={'100%'}
-        play={true}
-        onChangeState={onStateChange}
-        onReady={onReady}
-        loop={true}
+      videoId={url}
+      height={'100%'}
+      play={playing}
+      ref={playerRef}
+      onChangeState={onStateChange}
+      initialPlayerParams={{
+        start: 2,
+        end: 4,
+      }}
       />
     </View>
-    <ScrollView style={styles.scroll}
-      scrollEnabled={scrollEnabled.current}
-      contentContainerStyle={styles.content}
-    >
-      <Text style={{color: 'white'}}>{formatTime(startState)}</Text>
-      <MultiSlider
-      min={0}
-      max={duration}
-      values={[startState, endState]}
-      onValuesChange={onValuesChange}
-      // enableLabel={true}
-      // valuePrefix={'time'}
-      minMarkerOverlapDistance={1}
-      markerOffsetX={0}
-      markerOffsetY={15}
-      onValuesChangeStart={() => disableScroll()}
-      onValuesChangeFinish={() => enableScroll()}
-      enabledTwo={true}
-      allowOverlap={false}
-      customMarkerLeft={() => (<View style={{width: 10, height: 10, backgroundColor: 'red'}}/>)}
-      customMarkerRight={() => (<View style={{width: 10, height: 10, backgroundColor: 'yellow'}}/>)}
-      isMarkersSeparated={true}
-      trackStyle={{backgroundColor: 'pink'}}
-      markerStyle={{backgroundColor: 'red'}}
-      selectedStyle={{backgroundColor: 'blue'}}
-      />
-      <Text style={{color: 'white'}}>{formatTime(endState)}</Text>
-    
-      <Button
-        title="log details"
-        onPress={() => {
-          console.log("Duration", duration)
-          console.log("startState", startState)
-          console.log("endState", endState)
-        }}
-      />
-    </ScrollView>
     </>
   );
 }
