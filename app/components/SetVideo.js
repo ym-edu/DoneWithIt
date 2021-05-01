274916d8c24bf247ea07c70c56c595f85c3beeaf
nym <nym@nym.local>
2021-05-01 18:13:15 -0500

    Achieved a working solution for looping video with a combination of YTAPIv3 + YTIFrame; Get duration from APIv3 and conditionally render IFrame once duration is set. Then set a listener on component mount that checks video's current time. If time is greater than end param, go to start time defined by start param. If time is less thatn start param, go to start time defined by start param. Also if State is 'ended' got to start time defined by start param; User can pause and move slider as they please and video will loop as intended

--- app/components/SetVideo.js
+++ app/components/SetVideo.js
@@ -1,47 +1,70 @@
 import React, { useState, useCallback, useRef, useEffect } from "react";
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import YoutubePlayer from "react-native-youtube-iframe";
+import moment from "moment";
+import momentDurationFormatSetup from "moment-duration-format"; //Required
 
-function Video({url='DN0gAQQ7FAQ'}) {
+const formatDurationToS = (duration) => moment.duration(duration).format("s", {
+  groupingSeparator: "",
+});
+
+function Video({url = 'ZfawH9NsTtI'}) {
+  const [duration, setDuration] = useState(null);
+
   const playerRef = useRef();
-  const [playing, setPlaying] = useState(true);
-  const [playerState, setPlayerState] = useState('');
+  const [start, setStart] = useState(148);
+  const [end, setEnd] = useState(152);
 
-  const togglePlaying = useCallback(() => {
-    return !playing
-  }, [playing])
+  const fetchVideo = async () => {
+    const api = `https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&id=${url}&key=AIzaSyCedwcitDUV0CLExJpuGf269YAzaInjgkA`
+    const response = await fetch(api);
+    const json = await response.json();
+    return json
+  }
 
-  const onStateChange = useCallback((state) => {
-    setPlayerState(state)
+  useEffect(() => {
+    console.log('Fetching...')
+    fetchVideo().then(result => {
+      const PT = result.items[0].contentDetails.duration
+      const duration = parseInt(formatDurationToS(PT), 10)
+      setDuration(duration)
+    })
+
+    const interval = setInterval(() => {
+      playerRef.current?.getCurrentTime()
+      .then(currentTime => {
+        const time = Math.floor(currentTime)
+        if(time >= end || time < start) {
+          playerRef.current?.seekTo(start, true)
+        }
+      })
+    }, 200); //WARNING: a low enough value will return [MaxListenersExceededWarning] - e.g. for 100, 11 getCurrentTime listeners added
+
+    return () => clearInterval(interval);
   }, [])
 
-  useEffect(() => {
-    if(playerState === "ended" || playerState === "paused") {
-      setPlaying(togglePlaying())
+  const onStateChange = useCallback((state) => {
+    if(state === "ended") {
+      console.log(state)
+      playerRef.current?.seekTo(start, true)
     }
-  }, [playerState])
+  }, [])
 
-  useEffect(() => {
-    if(playerState === "ended" || playerState === "paused") {
-      setPlaying(true)
-    }
-  }, [togglePlaying])
-
- 
   return (
     <>
     <View style={styles.container}>
-      <YoutubePlayer
+      {duration && <YoutubePlayer
       videoId={url}
       height={'100%'}
-      play={playing}
       ref={playerRef}
+      play={true}
       onChangeState={onStateChange}
-      initialPlayerParams={{
-        start: 2,
-        end: 4,
-      }}
-      />
+      />}
+    </View>
+    <View style={{flex: 1}}>
+      <Text style={{color: 'white', alignSelf: 'center'}}>
+        {duration && duration}
+      </Text>
     </View>
     </>
   );
