35fbfe800d3b9781020b90c89d0e991e521263df
nym <nym@nym.local>
2021-05-16 22:10:19 -0500

    UPDATE: Workout screens in LibraryStack now have an Options icon at the top right of the stack header which navigates to a WorkoutOptions modal screen from RootStack; Global modal screens within RootStack share modalOptions object, however, modal input's background is opacity:1, while modal button's background is opacity:0.5 (distinction not necessary for ios); WorkoutOptions screen is just a list of buttons presented as a modal

--- app/routes/LibraryStack.js
+++ app/routes/LibraryStack.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Library from '../screens/LibraryTab';
 import Exercises from '../screens/Exercises';
 import Workout from '../screens/Workout';
+import Options from '../components/Options';
 
 const Stack = createStackNavigator();
 
@@ -29,13 +30,15 @@ export default function LibraryStack() {
         component={Exercises}
         options={{title: 'Exercises'}}
         />
-
-         <Stack.Screen
+        <Stack.Screen
         name='Workout'
         component={Workout}
-        options={({route}) => {
+        options={({ navigation, route }) => {
           return {
-            headerTitle: route.params.title
+            headerTitle: route.params.title,
+            headerRight: () => (
+            <Options onPress={() => navigation.navigate('WorkoutOptions')}/>
+            ),
           }
         }}
         />
