0ab0746fc0207e0dc3e90f8bc7d091baf587c1d9
nym <nym@nym.local>
2021-05-31 15:51:56 -0500

    UPDATE: Implemented an initializer function to set initial state based on exercise list

--- app/screens/Train.js
+++ app/screens/Train.js
@@ -1,11 +1,73 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { Button, StyleSheet, View } from 'react-native';
 
-function Train({navigation, route}) {
+const ACTIONS = {
+  REPS_FIXED: 'repsFixed',
+  TIME_FIXED: 'timeFixed',
+  REPS_TARGET: 'repsTarget',
+  TIME_TARGET: 'timeTarget',
+}
+
+function initializer(exercises) {
+  return exercises.map(({mode}) => {
+    switch(mode.current) {
+
+      case ACTIONS.REPS_FIXED:
+        return {
+          isStarting: true,
+          isFinished: false,
+          count: mode[mode.current],
+        }
+        
+      case ACTIONS.TIME_FIXED:
+        return {
+          isStarting: true,
+          isFinished: false,
+          count: mode[mode.current].min * 60 * 1000 + mode[mode.current].sec * 1000
+        }
+
+      case ACTIONS.REPS_TARGET:
+        return {
+          isStarting: true,
+          isFinished: false,
+          count: 0,
+        }
+
+      case ACTIONS.TIME_TARGET:
+        return ({
+          isStarting: true,
+          isFinished: false,
+          count: 0
+        })
+    }
+  });
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case ACTIONS.REPS_FIXED:
+      console.log(state)
+      return state
+    case ACTIONS.TIME_FIXED:
+      console.log(state)
+      return state
+    case ACTIONS.REPS_TARGET:
+      console.log(state)
+      return state
+    case ACTIONS.TIME_TARGET:
+      console.log(state)
+      return state
+  }
+}
+
+function Train({navigation, route:{params:{exercises}}}) {
+  const [state, dispatch] = useReducer(reducer, exercises, initializer)
+
   return (
     <>
       <View style={styles.container}>
-        {/* <Button title={'log'} onPress={() => {console.log(route.params.exercises)}}/> */}
+        <Button title={'log'} onPress={() => console.log(state)}/>
+        <Button title={'back'} onPress={() => navigation.pop()}/>
       </View>
     </>
   );
